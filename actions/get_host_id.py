06760c1ac5b9d2901459ee02b83b65b8b5360c90
jacker-lenovo <309824690@qq.com>
2018-06-01 14:40:03 +0800

    修正获取参数

--- actions/get_host_id.py
+++ actions/get_host_id.py
@@ -2,12 +2,11 @@ from pyzabbix import ZabbixAPI
 from st2common.runners.base_action import Action
 import json
 
-# import json
 
 class ZabbixHost(Action):
     def run(self):
-        zapi = ZabbixAPI(self.config.get('url'))
-        zapi.login(self.config.get('username'),self.config.get('password'))
+        zapi = ZabbixAPI(self.config['zabbix']['url'])
+        zapi.login(self.config['zabbix']['username'],self.config['zabbix']['password'])
         ret = zapi.do_request('host.get',{"output":["host"]})
         data = []
         for i in ret['result']:
